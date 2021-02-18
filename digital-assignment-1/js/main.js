7f7861e87b6d185d5ab7dc6bbc7879664d607392
nhan1407 <nhanle140796@gmail.com>
2021-02-18 13:40:17 -0500

    finish game 2

--- digital-assignment-1/js/main.js
+++ digital-assignment-1/js/main.js
@@ -10,136 +10,116 @@ import "./phaser.js";
 // All loading functions will typically all be found inside `preload()`.
 
 // The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
-var enemyReactionTimer;
+var sprite;
+var healthGroup;
+var text;
+var cursors;
+var currentHealth = 100;
+var maxHealth = 100;
+var timedEvent;
 
 class MyScene extends Phaser.Scene {
 
-    preload() {
-        // Load Background + Character
-        this.load.image( 'bg', 'assets/bg.png' );
-        this.load.image('ninja-1', 'assets/characters/idle-1.PNG');
-        this.load.image('ninja-2', 'assets/characters/idle-2.PNG');
-        this.load.image('dead-1', 'assets/characters/dead-1.PNG');
-        this.load.image('dead-2', 'assets/characters/dead-2.PNG');
-
-        // Load sound effects and bgm
-        this.load.audio('bgm','assets/audio/bgm.mp3');
-        this.load.audio('shuriken','assets/audio/shuriken.mp3');
-        this.load.audio('signal','assets/audio/slash.mp3');
-        this.load.audio('nope','assets/audio/nope.mp3');
-        console.log('Preload finished');
-
-        
-        }
-    
-    create() {
-
-
-        //creating bacground and character
-        this.add.image(500,150,'bg');
-        this.player = this.add.sprite(120, 480, 'ninja-1');
-        this.comp = this.add.sprite(680, 480, 'ninja-2');
-
-
-        //initialize buttons
-        this.attack = this.input.keyboard.addKey('SPACE');
-        this.reset = this.input.keyboard.addKey('R');
-
-        //adding sound effects
-        this.bgm = this.sound.add('bgm');
-        this.shuriken = this.sound.add('shuriken');
-        this.signalAudio = this.sound.add('signal');
-        this.nope = this.sound.add('nope');
-        this.bgm.play();
-        
-        
-
-        //generate a random number to used as a signal timer
-        this.randomNumber = Phaser.Math.Between(3,10);
-        console.log(" random number: " + this.randomNumber);
-        
-        // Seting enemy's reaction time.
-        this.enemyReactionTime = Phaser.Math.Between(7,12);
-
-        // setting condition for gameplay
-        this.player_isAlive = true;
-        this.comp_isAlive = true;
-        this.haveShuriken = true;
-        this.signal = false;
-        console.log("Finishing setup");
-
-        // initialize timer for for the signal
-        var signalTimer = this.time.delayedCall(this.randomNumber *1000,this.playSignal,[], this);
-
+    preload ()
+    {
+        this.load.image('cat', 'assets/characters/player_handgun.png');
+        this.load.image('health', 'assets/target.png');
     }
-    
-    update() {
 
-        // Print the tutorial
-        if(this.comp_isAlive ==  true && this.player_isAlive ==  true){
-            let style = { font: "30px Tahoma", fill: "#ffffff", align: "top" };
-            let text = this.add.text( this.cameras.main.centerX, this.cameras.main.centerY, "Press SPACE to Attack", style );
-            text.setOrigin( 0.5, 5 );
+    create ()
+    {
+        sprite = this.physics.add.image(400, 300, 'cat');
+
+        sprite.setCollideWorldBounds(true);
+
+        //  Create 10 random health pick-ups
+        healthGroup = this.physics.add.staticGroup({
+            key: 'health',
+            frameQuantity: 10,
+            immovable: true
+        });
+
+        var children = healthGroup.getChildren();
+
+        for (var i = 0; i < children.length; i++)
+        {
+            var x = Phaser.Math.Between(50, 750);
+            var y = Phaser.Math.Between(50, 550);
+
+            children[i].setPosition(x, y);
         }
 
-        // If player press SPACE before signal, they will lose the game
-        if( this.attack.isDown && this.signal == false){
-            this.nope.play();
-            this.haveShuriken = false;
-            console.log ("Wrong timming, You Lose");
-        }
-        // if play Press SPACE after the signal and before computer reaction. Player WIN.
-        if(this.attack.isDown && this.signal == true && this.player_isAlive == true){
-            //Get elapsed time to check player'sreaction time
-            var elapsed = enemyReactionTimer.getElapsed();
-            if (elapsed < 5000 && this.haveShuriken == true) {
-                this.shuriken.play();
-                this.cameras.main.flash();
-                this.haveShuriken = false;
-                this.comp_isAlive = false;
-                enemyReactionTimer.remove();
-                this.comp.setTexture('dead-2');
-                console.log("Player Win" + elapsed);
-            }
-        }
-        // if player lose, player ninja falls
-        if (this.player_isAlive == false && this.player.angle >-90) {
-            this.player.angle -=5;
-            let style = { font: "30px Tahoma", fill: "#ffffff", align: "center" };
-            let text = this.add.text( this.cameras.main.centerX, this.cameras.main.centerY, "YOU LOSE\nPress R to play again", style );
-            text.setOrigin( 0.5, 0.0 );
-        }
-        //otherwise, computer ninja falls
-        if(this.comp_isAlive ==  false && this.comp.angle <90){
-            this.comp.angle +=5;
-            let style = { font: "30px Tahoma", fill: "#ffffff", align: "center" };
-            let text = this.add.text( this.cameras.main.centerX, this.cameras.main.centerY, "YOU WIN\nPress R to play again", style );
-            text.setOrigin( 0.5, 0.0 );
-        }
-        // Play again when player press R.
-        if(this.reset.isDown){
-            this.scene.restart();
-        }
-         
+        healthGroup.refresh();
 
+        //  So we can see how much health we have left
+        text = this.add.text(10, 10, 'Health: 100', { font: '32px Courier', fill: '#000000' });
+
+        //  Cursors to move
+        cursors = this.input.keyboard.createCursorKeys();
+
+        //  When the player sprite his the health packs, call this function ...
+        this.physics.add.overlap(sprite, healthGroup, spriteHitHealth);
+
+        //  Decrease the health by calling reduceHealth every 50ms
+        timedEvent = this.time.addEvent({ delay: 50, callback: reduceHealth, callbackScope: this, loop: true });
     }
-    // play signal sound effect and start the reaction timer.
-    playSignal(){
-        this.signalAudio.play();
-        this.signal = true;
-        enemyReactionTimer = this.time.delayedCall(this.enemyReactionTime*100,this.computerWin,[], this);
-        console.log("Dueling time " +this.enemyReactionTime);
+
+    reduceHealth ()
+    {
+        currentHealth--;
+
+        if (currentHealth === 0)
+        {
+            //  Uh oh, we're dead
+            sprite.body.reset(400, 300);
+
+            text.setText('Health: RIP');
+
+            //  Stop the timer
+            timedEvent.remove();
+        }
     }
-    // If the computer's timer run out of time, which means player lose. This function will execute. 
-    computerWin(){
-        this.player_isAlive = false; 
-        this.shuriken.play();
-        this.haveShuriken = false;
-        this.cameras.main.flash();
-        this.player.setTexture('dead-1');
-        enemyReactionTimer.remove();
-        console.log("Computer Win");
-        
+
+    spriteHitHealth (sprite, health)
+    {
+        //  Hide the sprite
+        healthGroup.killAndHide(health);
+
+        //  And disable the body
+        health.body.enable = false;
+
+        //  Add 10 health, it'll never go over maxHealth
+        currentHealth = Phaser.Math.MaxAdd(currentHealth, 10, maxHealth);
+    }
+
+    update ()
+    {
+        if (currentHealth === 0)
+        {
+            return;
+        }
+
+        text.setText('Health: ' + currentHealth);
+
+        sprite.setVelocity(0);
+
+        if (cursors.left.isDown)
+        {
+            sprite.setVelocityX(-200);
+        }
+        else if (cursors.right.isDown)
+        {
+            sprite.setVelocityX(200);
+        }
+
+        if (cursors.up.isDown)
+        {
+            sprite.setVelocityY(-200);
+        }
+        else if (cursors.down.isDown)
+        {
+            sprite.setVelocityY(200);
+        }
     }
 }
 
--- digital-assignment-2/js/main.js
+++ digital-assignment-2/js/main.js
@@ -10,356 +10,194 @@ import "./phaser.js";
 // All loading functions will typically all be found inside `preload()`.
 
 // The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+var sprite;
+var healthGroup;
+var text;
+var cursors;
+var currentHealth = 100;
+var maxHealth = 100;
+var timedEvent;
+var oasisTimedEvent;
+var spawnOasisTime;
+var oasis;
+var gameOver = false;
+var bgm;
+var pick;
+var lose;
+var win;
 
-var config = {
-    type: Phaser.AUTO,
-    parent: 'game',
-    backgroundColor: '#3A3D3F',
-    width: 800,
-    height: 600,
-    physics: { 
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
 
-    },
-    scene: {
-        preload : preload,
-        create : create, 
-        update : update,
-        extend : {
-            player: null, 
-            healthpoints: null,
-            reticle: null,
-            moveKeys: null, 
-            playerBullets: null,
-            enemyBullets: null,
-            time: 0,
-        }
+class MyScene extends Phaser.Scene {
+
+    preload ()
+    {
+        // preload image
+        this.load.image('cat', 'assets/characters/player_handgun.png');
+        this.load.image('health', 'assets/pt.png');
+        this.load.image('bg', 'assets/background/sand.jpg');
+        this.load.image('oasis', 'assets/oasis.png');
+
+        // preload sound
+        this.load.audio('bgm','assets/audio/bgm2.mp3');
+        this.load.audio('pick','assets/audio/pick.mp3');
+        this.load.audio('win','assets/audio/classic-song.mp3');
+        this.load.audio('lose','assets/audio/sad-song.mp3');
+
     }
-};
-var playerBullets;
-var enemyBullets;
-var player;
-var enemy;
-var reticle;
-var hp1;
-var hp2;
-var hp3;
-var moveKeys;
-var playerHealth = 3;
-var enemyHealth = 3;
-var cursorKeys;
 
-var game = new Phaser.Game(config);
-var Bullet = new Phaser.Class({
-    Extends: Phaser.GameObjects.Image,
-    
-    initialize:
-
-    // Bullet Constructor
-    function Bullet (scene){
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
-        this.speed = 1;
-        this.born = 0;
-        this.direction = 0;
-        this.xSpeed = 0;
-        this.ySpeet = 0;
-        this.setSize (12, 12, true);
-    },
-    // Fire a bullet from the playter to the reticle 
-    fire: function (shooter, target)
+    create ()
     {
-        this.setPosition(shooter.x, shooter.y); // Initial position 
-        this.direction = Math.atan ((target.x - this.x) / (target.y - this.y));
+        //add sounds
+        bgm = this.sound.add('bgm');
+        pick = this.sound.add('pick');
+        win = this.sound.add ('win');
+        lose = this.sound.add('lose');
+        bgm.play();
 
-        // Calculate X and Y velocity of bullet to move it from shooter to target
-        if (target.y >= this.y){
-            this.xSpeed = this.speed * Math.sin(this.direction);
-            this.ySpeed = this.speed * Math.cos(this.direction);
-        }
-        else {
-            this.xSpeed = -this.speed * Math.sin(this.direction);
-            this.ySpeed = -this.speed * Math.cos(this.direction);
-        }
+        this.add.image(0,0,'bg');
+        sprite = this.physics.add.image(400, 300, 'cat');
 
-        this.rotation = shooter.rotation // angle bullet with shooters rotation
-        this.born =0; //Time since new bullet spawned
-    },
-    // Updates the position of the bullet each cycle
-    update: function (time,delta)
-    {
-        this.x += this.xSpeed * delta;
-        this.y += this.ySpeed * delta;
-        this.born += delta;
-        if (this.born > 1800)
+        sprite.setCollideWorldBounds(true);
+
+        oasis = this.physics.add.group();
+
+        //  Create 10 random health pick-ups
+        healthGroup = this.physics.add.staticGroup({
+            key: 'health',
+            frameQuantity: 10,
+            immovable: true
+        });
+
+        var children = healthGroup.getChildren();
+
+        for (var i = 0; i < children.length; i++)
         {
-            this.setActive(false);
-            this.setVisible(false);
+            var x = Phaser.Math.Between(50, 750);
+            var y = Phaser.Math.Between(50, 550);
+
+            children[i].setPosition(x, y);
         }
+
+        healthGroup.refresh();
+
+        //  So we can see how much health we have left
+        text = this.add.text(10, 10, 'Health: 100', { font: '32px Courier', fill: '#000000' });
+
+        //  Cursors to move
+        cursors = this.input.keyboard.createCursorKeys();
+
+        //  When the player sprite his the health packs, call this function ...
+        this.physics.add.overlap(sprite, healthGroup, spriteHitHealth);
+
+
+
+        // Spawn Oasis after a time
+        spawnOasisTime = Phaser.Math.Between(4, 8);
+        console.log('spawn oasis in ' + spawnOasisTime);
+        oasisTimedEvent = this.time.addEvent({ delay: spawnOasisTime*1000, callback: spawnOasis, callbackScope: this, loop: false });
+
+        //  When the player sprite his the oasis, call this function ...
+        this.physics.add.overlap(sprite, oasis, spriteHitOasis);
+        
+        //  Decrease the health by calling reduceHealth every 50ms
+        timedEvent = this.time.addEvent({ delay: 50, callback: reduceHealth, callbackScope: this, loop: true });
+
     }
-});
 
-function preload() {
-    // Load Background + Character
-    this.load.image( 'ranger', 'assets/characters/player_handgun.png',
-        { frameWidth: 66, frameHeight: 60 });
-    this.load.image('bullet', 'assets/bullet/bullet6.png');
-    this.load.image('target', 'assets/target.png');
-    
+    update ()
+    {
 
-    // Load sound effects and bgm
-    //this.load.audio('bgm','assets/audio/bgm.mp3');
-    
-    console.log('Preload finished');
+        if (gameOver === true){
 
-    }
-
-function create() {
-    
-    var outer = new Phaser.Geom.Rectangle(0, 0, 800, 600);
-    var inner = new Phaser.Geom.Rectangle(350, 250, 100, 100);
-    
-    // Add Bullet Objects
-    playerBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true});
-    enemyBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
-    
-    // Set world bounds
-    this.physics.world.setBounds(0, 0, 1600, 1200);
-
-    // Add background, player, enemy, reticle, healthpoint, sprites
-    player = this.physics.add.image(800,600,'ranger');
-    enemy = this.physics.add.sprite(400,300,'ranger');
-    reticle = this.physics.add.sprite(800, 700, 'target');
-    
-
-    //enemy = this.physics.add.group({ collideWorldBounds: true});
-    enemy.setOrigin(0.5, 0.5).setDisplaySize(132, 120);
-
-    this.physics.moveToObject(enemy, player, 200);
-
-
-    // for (var i = 0; i < 5; i++)
-    // {
-    //     var p = Phaser.Geom.Rectangle.RandomOutside(outer, inner);
-    //     var  = enemy.create(p.x, p.y, 'ranger');
-
-    //     this.physics.add.existing(b);
-
-    //     //b.body.setImmovable();
-    // }    
-
-    // Set image/Sprite properties
-    player.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true).setDrag(500, 500);
-    reticle.setOrigin(0.5, 0.5).setDisplaySize(25, 25).setCollideWorldBounds(true);
-
-    // set sprite variables
-
-    this.lastFired = 0;
-
-    // Set camera properties
-    this.cameras.main.zoom = 0.5;
-    this.cameras.main.startFollow(player);
-
-    // Enables movement of player with WASD keys
-    // cursorKeys = this.input.keyboard.createCursorKeys();
-    // cursorKeys = this.input.keyboard.addKeys(
-    // {
-    //     up:Phaser.Input.Keyboard.KeyCodes.W,
-    //     down:Phaser.Input.Keyboard.KeyCodes.S,
-    //     left:Phaser.Input.Keyboard.KeyCodes.A,
-    //     right:Phaser.Input.Keyboard.KeyCodes.D
-    // });
-
-
-    //Fires bullet from player on left click of mouse
-    this.input.on('pointerdown', function (pointer, time, lastFired){
-        if (player.active === false){
+            return;
+        }   
+        if (currentHealth === 0)
+        {
             return;
         }
 
-        // Get bullet from bullets group
-        var bullet = playerBullets.get().setActive(true).setVisible(true);
+        text.setText('Health: ' + currentHealth);
 
-        if(bullet)
+        sprite.setAcceleration(0);
+
+        if (cursors.left.isDown)
         {
-            bullet.fire(player, reticle);
-            this.physics.add.collider(enemy, bullet, enemyHitCallback);
+            sprite.setAccelerationX(-200);
         }
-    }, this);
+        else if (cursors.right.isDown)
+        {
+            sprite.setAccelerationX(200);
+        }
 
-    // Pointer lock will only work after mousedown
-    game.canvas.addEventListener('mousedown', function (){
-        game.input.mouse.requestPointerLock();
+        if (cursors.up.isDown)
+        {
+            sprite.setAccelerationY(-200);
+        }
+        else if (cursors.down.isDown)
+        {
+            sprite.setAccelerationY(200);
+        }
+    }
+}
+function spawnOasis(){
+    oasisTimedEvent.remove(); 
+    var x = Phaser.Math.Between(50, 750);
+    var y = Phaser.Math.Between(50, 550);
+    oasis.create(x, y, 'oasis');
+}
+
+function reduceHealth ()
+{
+    currentHealth --;
+
+    if (currentHealth === 0)
+    {
+        //  Uh oh, we're dead
+        sprite.body.reset(400, 300);
+
+        text.setText('You run out of water. ');
+        bgm.pause();
+        lose.play();
+        //  Stop the timer
+        timedEvent.remove();
+        oasisTimedEvent.remove();
+    }
+}
+
+function spriteHitHealth (sprite, health)
+{
+    //  Hide the sprite
+    healthGroup.killAndHide(health);
+    pick.play();
+    //  And disable the body
+    health.body.enable = false;
+
+    //  Add 10 health, it'll never go over maxHealth
+    currentHealth = Phaser.Math.MaxAdd(currentHealth, 10, maxHealth);
+
+    if (healthGroup.countActive(true) === 0) {
+        healthGroup.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, child.y, true, true);
+
+        });
+    }
+}
+function spriteHitOasis(sprite){
+    text.setText('You found the oasis.');
+    sprite.body.reset(400, 300);
+    bgm.pause();
+    win.play();     
+    timedEvent.remove();
+    oasisTimedEvent.remove();
+    gameOver = true;
+}
+
+const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 600,
+    scene: MyScene,
+    physics: { default: 'arcade' },
     });
-
-    // Exit pointer lock when Q or escape (by default) is pressed
-    this.input.keyboard.on('keydown_Q', function (event){
-        if (game.input.mouse.locked)
-            game.input.mouse.releasaePointerLock();
-    }, 0, this);
-
-    // Move reticle upon locked pointer move
-    this.input.on('pointermove', function (pointer){
-        if (this.input.mouse.locked)
-        {
-            reticle.x += pointer.movementX;
-            reticle.y += pointer.movementY;
-        }
-    }, this);
-    console.log("finish Creating");
-}
-
-function enemyHitCallback(enemyHit, bulletHit)
-{
-    // Reduce health of enemy
-    if(bulletHit.active === true && enemyHit.active === true)
-    {
-        enemyHealth -= 1;
-        //console.log("Enemy hp: ", enemyHealth);
-
-        // KIll enemy if health <= 0
-        if (enemyHealth <= 0)
-        {
-            enemyHit.setActive(false).setVisible(false);
-        }
-
-        // Destroy bullet
-        bulletHit.setActive(false).setVisible(false);
-    }
-}
-
-function playerHitCallback(playerHit, bulletHit)
-{
-    // Reduce health of player
-    if (bulletHit.active === true && playerHit.active === true)
-    {
-        playerHealth -=1;
-        console.log("Player hp: ", playerHealth);
-        if (playerHealth === 0)
-        {
-            this.physics.pause();
-            // Game over state should execute here
-        }
-
-        // Destroy bullet
-        bulletHit.setActive(false).setVisible(false);
-    }
-}
-
-function constrainVelocity(sprite, maxVelocity)
-{
-    if (!sprite || !sprite.body)
-      return;
-
-    var angle, currVelocitySqr, vx, vy;
-    vx = sprite.body.velocity.x;
-    vy = sprite.body.velocity.y;
-    currVelocitySqr = vx * vx + vy * vy;
-
-    if (currVelocitySqr > maxVelocity * maxVelocity)
-    {
-        angle = Math.atan2(vy, vx);
-        vx = Math.cos(angle) * maxVelocity;
-        vy = Math.sin(angle) * maxVelocity;
-        sprite.body.velocity.x = vx;
-        sprite.body.velocity.y = vy;
-    }
-}
-
-function constrainReticle(reticle)
-{
-    var distX = reticle.x-player.x; // X distance between player & reticle
-    var distY = reticle.y-player.y; // Y distance between player & reticle
-
-    // Ensures reticle cannot be moved offscreen (player follow)
-    if (distX > 800)
-        reticle.x = player.x+800;
-    else if (distX < -800)
-        reticle.x = player.x-800;
-
-    if (distY > 600)
-        reticle.y = player.y+600;
-    else if (distY < -600)
-        reticle.y = player.y-600;
-}
-
-function movePlayerManager()
-{
-    if (cursorKeys.up.isDown) 
-    {
-        player.setVelocity(-800);
-    }
-    else if (cursorKeys.down.isDown) 
-    {
-        player.setVelocity(800);
-    }
-    else {
-        player.setVelocity(0);
-    }
-    
-    if (cursorKeys.left.isDown) 
-    {
-        player.setVelocity(-800);
-    }
-    else if (cursorKeys.right.isDown) 
-    {
-        player.setVelocity(800);
-    }
-    else {
-        player.setVelocity(0);
-    }
-
-}
-
-function enemyFire(enemy, player, time, gameObject)
-{
-    if (enemy.active === false)
-    {
-        return;
-    }
-
-    if ((time - enemy.lastFired) > 1000)
-    {
-        enemy.lastFired = time;
-
-        // Get bullet from bullets group
-        var bullet = enemyBullets.get().setActive(true).setVisible(true);
-
-        if (bullet)
-        {
-            bullet.fire(enemy, player);
-            // Add collider between bullet and player
-            gameObject.physics.add.collider(player, bullet, playerHitCallback);
-        }
-    }
-}
-
-function update (time, delta)
-{
-    //movePlayerManager();
-
-    // Rotates player to face towards reticle
-    player.rotation = Phaser.Math.Angle.Between(player.x, player.y, reticle.x, reticle.y);
-
-    // Rotates enemy to face towards player
-    enemy.rotation = Phaser.Math.Angle.Between(enemy.x, enemy.y, player.x, player.y);
-
-    //Make reticle move with player
-    reticle.body.velocity.x = player.body.velocity.x;
-    reticle.body.velocity.y = player.body.velocity.y;
-
-    // Constrain velocity of player
-    constrainVelocity(player, 500);
-
-    // Constrain position of constrainReticle
-    constrainReticle(reticle);
-
-    // Make enemy fire
-    enemyFire(enemy, player, time, this);
-
-}
-
-
